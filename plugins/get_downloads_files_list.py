2e33df9c9174de47bcea53f14d907447862e5f0b
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-10 20:12:41 +0300

    Update get_downloads_files_list.py

--- plugins/get_downloads_files_list.py
+++ plugins/get_downloads_files_list.py
@@ -4,6 +4,8 @@ from pyrogram import Client, filters
 from config import Config
 
 directory = f"{Config.DOWNLOAD_DIR}/"
+directorye = f"{Config.ENCODE_DIR}/"
+
 
 @Client.on_message(filters.command('downloads'))
 async def downloads_list(client, message):
@@ -11,7 +13,7 @@ async def downloads_list(client, message):
     dsy = ""
     if 1 == 1:
         if not os.listdir(directory):
-            await message.reply("Combo klasörünüz boş")
+            await message.reply("İndirilenler klasörünüz boş")
         else:
             for files in os.listdir(directory):
                 say = say + 1
@@ -19,3 +21,19 @@ async def downloads_list(client, message):
             await message.reply_text(
                 "İndirilenler Listesi" + "\n\n" + dsy + "\n" + str(
                     say) + " Bu Kadar İndirilen Dosyan Var.")
+
+
+@Client.on_message(filters.command('encodes'))
+async def encodes_list(client, message):
+    say = 0
+    dsy = ""
+    if 1 == 1:
+        if not os.listdir(directory):
+            await message.reply("Encode klasörünüz boş")
+        else:
+            for files in os.listdir(directorye):
+                say = say + 1
+                dsy = dsy + "	" + str(say) + "-) " + files + '\n'
+            await message.reply_text(
+                "İndirilenler Listesi" + "\n\n" + dsy + "\n" + str(
+                    say) + " Bu Kadar Encode Dosyan Var.")
