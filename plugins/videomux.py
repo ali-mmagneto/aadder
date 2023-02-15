51b5359588551700f85c02d1833183c5de3ea007
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-15 11:15:36 +0300

    Update videomux.py

--- plugins/videomux.py
+++ plugins/videomux.py
@@ -8,9 +8,11 @@ from helper_func.thumb import get_thumbnail, get_duration, get_width_height
 from helper_func.progress_bar import progress_bar
 from config import Config
 import time
+import random
 
 @Client.on_message(filters.command('video1'))
 async def video1al(bot, message):
+    rand = random.randint(10, 900) 
     msg = await bot.send_message(
         chat_id=message.chat.id,
         text="`İşlem Başlatıldı...`")
@@ -18,14 +20,15 @@ async def video1al(bot, message):
     start_time = time.time()
     media = await bot.download_media(
                 message = message.reply_to_message,
-                file_name = f"downloads/video1.mp4",
+                file_name = f"downloads/video1-{rand}.mp4",
                 progress=progress_bar,
                 progress_args=("`İndiriliyor...`", msg, start_time))
-    old_file_name = "downloads/video1.mp4"
+    old_file_name = "downloads/video1-{rand}.mp4"
     await message.reply_video(old_file_name)
 
 @Client.on_message(filters.command('video2'))
 async def video2al(bot, message):
+    rand = random.randint(10, 900) 
     msg = await bot.send_message(
         chat_id=message.chat.id,
         text="`İşlem Başlatıldı...`")
@@ -33,8 +36,8 @@ async def video2al(bot, message):
     start_time = time.time()
     media = await bot.download_media(
                 message = message.reply_to_message,
-                file_name = f"downloads/video2.mp4",
+                file_name = f"downloads/video2-{rand}.mp4",
                 progress=progress_bar,
                 progress_args=("`İndiriliyor...`", msg, start_time))
-    old_file_name = "downloads/video2.mp4"
+    old_file_name = f"downloads/video2-{rand}.mp4"
     await message.reply_video(old_file_name)
